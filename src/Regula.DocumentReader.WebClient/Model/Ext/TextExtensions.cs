56cead211b7924b453fb40e4df7c057898458dda
Hleb Albau <hleb.albau@gmail.com>
2020-11-06 22:23:46 +0300

    Add text extractions methods

--- src/Regula.DocumentReader.WebClient/Model/Ext/TextExtensions.cs
+++ src/Regula.DocumentReader.WebClient/Model/Ext/TextExtensions.cs
@@ -2,26 +2,71 @@ namespace Regula.DocumentReader.WebClient.Model.Ext
 {
     public static class TextExtensions
     {
-        public static TextField GetField(this Text txt, int fieldType, int lcid = 0)
+        
+        public static TextField GetField(this Text txt, int fieldType)
+        {
+            TextField result = null;
+            foreach (var field in txt.FieldList)
+            {
+                if (field.FieldType == fieldType && field.Lcid==0)
+                    return field;
+                if (result == null && field.FieldType == fieldType)
+                    result = field;
+            }
+            return result;
+        }
+
+        public static TextField GetField(this Text txt, int fieldType, int lcid)
         {
             foreach (var field in txt.FieldList)
             {
                 if (field.FieldType == fieldType && field.Lcid == lcid)
                     return field;
             }
-
             return null;
         }
         
-        public static string GetFieldValue(this Text txt, int fieldType, int lcid = 0)
+        public static TextField GetField(this Text txt, string fieldName)
+        {
+            TextField result = null;
+            foreach (var field in txt.FieldList)
+            {
+                if (field.FieldName == fieldName && field.Lcid==0)
+                    return field;
+                if (result == null && field.FieldName == fieldName)
+                    result = field;
+            }
+            return result;
+        }
+        
+        public static TextField GetField(this Text txt, string fieldName, int lcid)
         {
             foreach (var field in txt.FieldList)
             {
-                if (field.FieldType == fieldType && field.Lcid == lcid)
-                    return field.Value;
+                if (field.FieldName == fieldName && field.Lcid == lcid)
+                    return field;
             }
-
             return null;
+        }
+        
+        public static string GetFieldValue(this Text txt, int fieldType)
+        {
+            return txt.GetField(fieldType)?.Value;
+        }
+        
+        public static string GetFieldValue(this Text txt, int fieldType, int lcid)
+        {
+            return txt.GetField(fieldType, lcid)?.Value;
+        }
+        
+        public static string GetFieldValue(this Text txt, string fieldName)
+        {
+            return txt.GetField(fieldName)?.Value;
+        }
+        
+        public static string GetFieldValue(this Text txt, string fieldName, int lcid)
+        {
+            return txt.GetField(fieldName, lcid)?.Value;
         }
     }
 }
