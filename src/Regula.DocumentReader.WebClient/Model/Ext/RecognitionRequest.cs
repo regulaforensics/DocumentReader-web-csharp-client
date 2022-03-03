0592b51c31be70cd8d767c3b554ae08b5185a450
Danila Kislitsyn <dangost16@gmail.com>
2022-03-03 15:18:59 +0300

    fix ext class

--- src/Regula.DocumentReader.WebClient/Model/Ext/RecognitionRequest.cs
+++ src/Regula.DocumentReader.WebClient/Model/Ext/RecognitionRequest.cs
@@ -4,20 +4,20 @@ namespace Regula.DocumentReader.WebClient.Model.Ext
 {
     public class RecognitionRequest : ProcessRequest
     {
-        public RecognitionRequest(ProcessParams param, ProcessRequestImage image)
-            :base(param, new List<ProcessRequestImage> { image })
+        public RecognitionRequest(ProcessParams param, ProcessRequestImage image, string tag=null)
+            :base(tag, param, new List<ProcessRequestImage> { image })
         { }
         
-        public RecognitionRequest(ProcessParams param, byte[] image)
-            :base(param, new List<ProcessRequestImage> {new ProcessRequestImage(image)})
+        public RecognitionRequest(ProcessParams param, byte[] image, string tag=null)
+            :base(tag, param, new List<ProcessRequestImage> {new ProcessRequestImage(image)})
         { }
         
-        public RecognitionRequest(ProcessParams param, List<ProcessRequestImage> images)
-            :base(param, images)
+        public RecognitionRequest(ProcessParams param, List<ProcessRequestImage> images, string tag=null)
+            :base(tag, param, images)
         { }
 
-        public RecognitionRequest(ProcessParams param, ContainerList containerList)
-            : base(param, null, containerList)
+        public RecognitionRequest(ProcessParams param, ContainerList containerList, string tag=null)
+            : base(tag, param, null, containerList)
         { }
     }
 }
