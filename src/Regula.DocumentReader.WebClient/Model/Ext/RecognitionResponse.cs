50621c0a0b0e42e239ef604695e55cbab409a10a
Danila Kislitsyn <59931607+dangost@users.noreply.github.com>
2023-08-29 16:31:08 +0300

    decompress log (#75)

--- src/Regula.DocumentReader.WebClient/Model/Ext/RecognitionResponse.cs
+++ src/Regula.DocumentReader.WebClient/Model/Ext/RecognitionResponse.cs
@@ -1,5 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Text;
 
 namespace Regula.DocumentReader.WebClient.Model.Ext
 {
@@ -47,6 +50,24 @@ namespace Regula.DocumentReader.WebClient.Model.Ext
         {
             var result = ResultByType<ChosenDocumentTypeResult>(Result.DOCUMENT_TYPE, pageIdx);
             return result?.OneCandidate;
+        }
+
+        public string Log()
+        {
+            string logBase64 = this.OriginalResponse.Log;
+            if (logBase64 == null)
+            {
+                return null;
+            }
+            
+            byte[] buffer = Convert.FromBase64String(logBase64);
+        
+            using (MemoryStream memoryStream = new MemoryStream(buffer))
+            using (DeflateStream deflateStream = new DeflateStream(memoryStream, CompressionMode.Decompress))
+            using (StreamReader streamReader = new StreamReader(deflateStream, Encoding.UTF8))
+            {
+                return streamReader.ReadToEnd();
+            }
         }
 
         public T ResultByType<T>(int type, int pageIdx=0) where T: ResultItem
