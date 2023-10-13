cbf99d1793f2803cacd2dd1ae2eb553314601aa7
Alexander Gubin <aliaksandr.hubin@regula.us>
2023-10-13 18:51:07 +0300

    add process async (#89)
    * add process async
    
    * add process async

--- src/Regula.DocumentReader.WebClient/Api/DocumentReaderApi.cs
+++ src/Regula.DocumentReader.WebClient/Api/DocumentReaderApi.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
 using Regula.DocumentReader.WebClient.Client;
 using Regula.DocumentReader.WebClient.Model;
 using Regula.DocumentReader.WebClient.Model.Ext;
@@ -24,7 +26,9 @@ namespace Regula.DocumentReader.WebClient.Api
         }
 
         private string License { get; set; }
-        
+
+
+
         public RecognitionResponse Process(ProcessRequest processRequest)
         {
             return Process(processRequest, new Dictionary<String, String>(), default(string));
@@ -49,6 +53,35 @@ namespace Regula.DocumentReader.WebClient.Api
 
             return new RecognitionResponse(this._processApi.ApiProcess(processRequest, headers, xRequestID));
         }
+
+
+        public async Task<RecognitionResponse> ProcessAsync(ProcessRequest processRequest)
+        {
+            return await ProcessAsync(processRequest, new Dictionary<String, String>(), default(string));
+        }
+
+        public async Task<RecognitionResponse> ProcessAsync(ProcessRequest processRequest, Dictionary<String, String> headers)
+        {
+            return await ProcessAsync(processRequest, headers, default(string));
+        }
+
+        public async Task<RecognitionResponse> ProcessAsync(ProcessRequest processRequest, String xRequestID)
+        {
+            return await ProcessAsync(processRequest, new Dictionary<String, String>(), xRequestID);
+        }
+
+        public async Task<RecognitionResponse> ProcessAsync(ProcessRequest processRequest, Dictionary<String, String> headers, String xRequestID, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            if (processRequest.SystemInfo == null)
+                processRequest.SystemInfo = new ProcessSystemInfo(License);
+            else
+                processRequest.SystemInfo.License = License;
+
+            var response = await this._processApi.ApiProcessAsync(processRequest, headers, xRequestID, cancellationToken);
+
+            return new RecognitionResponse(response);
+        }
+
         public DeviceInfo Ping(string xRequestID)
         {
             return this._defaultApi.Ping(new Dictionary<String, String>(), xRequestID);
