9dcf80321d328c20b68ce8cc60cd3a228089ebe5
GitHub Action <action@github.com>
2026-05-19 16:38:21 +0000

    Commit: 5efaff8b

--- src/Regula.DocumentReader.WebClient/Model/EncryptedRCLItem.cs
+++ src/Regula.DocumentReader.WebClient/Model/EncryptedRCLItem.cs
@@ -56,7 +56,7 @@ namespace Regula.DocumentReader.WebClient.Model
         /// </summary>
         /// <value>Base64 encoded data</value>
         /*
-        <example>[B@11e8e013</example>
+        <example>[B@3b6ff8b0</example>
         */
         [DataMember(Name = "EncryptedRCL", IsRequired = true, EmitDefaultValue = true)]
         public byte[] EncryptedRCL { get; set; }
--- src/Regula.DocumentReader.WebClient/Model/LicenseItem.cs
+++ src/Regula.DocumentReader.WebClient/Model/LicenseItem.cs
@@ -56,7 +56,7 @@ namespace Regula.DocumentReader.WebClient.Model
         /// </summary>
         /// <value>Base64 encoded data</value>
         /*
-        <example>[B@3200e287</example>
+        <example>[B@759cd0b8</example>
         */
         [DataMember(Name = "License", IsRequired = true, EmitDefaultValue = true)]
         public byte[] License { get; set; }
