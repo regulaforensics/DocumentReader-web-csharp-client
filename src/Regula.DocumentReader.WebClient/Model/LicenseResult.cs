c1fac73f079f13cb39b4e244f38085e09733364b
GitHub Action <action@github.com>
2026-08-31 11:01:58 +0000

    Commit: 06bfea98

--- src/Regula.DocumentReader.WebClient/Model/LicenseResult.cs
+++ src/Regula.DocumentReader.WebClient/Model/LicenseResult.cs
@@ -62,7 +62,7 @@ namespace Regula.DocumentReader.WebClient.Model
         /// </summary>
         /// <value>Base64 encoded data</value>
         /*
-        <example>[B@7d0d6740</example>
+        <example>[B@156eaa16</example>
         */
         [DataMember(Name = "License", IsRequired = true, EmitDefaultValue = true)]
         public byte[] License { get; set; }
