ef7c2a31c12f935ebae9e3e6fb5019a5003ac5e8
GitHub Action <action@github.com>
2026-08-31 10:59:42 +0000

    Commit: adaa6b36

--- src/Regula.DocumentReader.WebClient/Model/EncryptedRCLResult.cs
+++ src/Regula.DocumentReader.WebClient/Model/EncryptedRCLResult.cs
@@ -62,7 +62,7 @@ namespace Regula.DocumentReader.WebClient.Model
         /// </summary>
         /// <value>Base64 encoded data</value>
         /*
-        <example>[B@1d47123</example>
+        <example>[B@5ed5b27c</example>
         */
         [DataMember(Name = "EncryptedRCL", IsRequired = true, EmitDefaultValue = true)]
         public byte[] EncryptedRCL { get; set; }
